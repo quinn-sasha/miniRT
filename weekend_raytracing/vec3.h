ae1f62151762921de27c165ffcd6710dcda49d95
quinn-sasha <s.kuin.24.5.1435@4nonome.com>
2025-12-01 18:17:11 +0900

    Added vec3 structure

---
+++ weekend_raytracing/vec3.h
@@ -0,0 +1,14 @@
+#ifndef VEC3_H
+#define VEC3_H
+
+#include <math.h> // needs -lm compile flagas
+#include <stdio.h>
+#include <unistd.h>
+
+typedef struct s_vec3 {
+  double x;
+  double y;
+  double z;
+} t_vec3;
+
+#endif
