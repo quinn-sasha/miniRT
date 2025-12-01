c7628ccee0b2c98c4a69e762b0bfadfd67d56a11
quinn-sasha <s.kuin.24.5.1435@4nonome.com>
2025-12-02 08:52:24 +0900

    Added ray structure

---
+++ include/ray.h
@@ -0,0 +1,13 @@
+#ifndef RAY_H
+#define RAY_H
+
+#include "vec3.h"
+
+typedef struct s_ray {
+  t_vec3 origin;
+  t_vec3 direction;
+} t_ray;
+
+t_vec3 ray_at(t_ray ray, double t);
+
+#endif // !RAY_H
---
+++ src/ray.c
@@ -0,0 +1,5 @@
+#include "ray.h"
+
+t_vec3 ray_at(t_ray ray, double t) {
+  return vec3_add(ray.origin, vec3_scale(ray.direction, t));
+}
