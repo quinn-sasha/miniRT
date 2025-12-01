01571f67b97ced860786fa4afbdd4046704f3468
quinn-sasha <s.kuin.24.5.1435@4nonome.com>
2025-12-01 20:31:07 +0900

    ディレクトリを再構築した

---
+++ include/color.h
@@ -0,0 +1,10 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+typedef struct s_color {
+  double red;
+  double green;
+  double blue;
+} t_color;
+
+#endif
---
+++ include/raytracing.h
@@ -0,0 +1,10 @@
+#ifndef RAYTRACING_H
+#define RAYTRACING_H
+
+#include "color.h"
+#include "vec3.h"
+#include <math.h> // needs -lm compile flagas
+#include <stdio.h>
+#include <unistd.h>
+
+#endif
