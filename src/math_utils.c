55bdbab327d60759d9264c8c78220186affcfc98
quinn-sasha <s.kuin.24.5.1435@4nonome.com>
2025-12-08 22:35:03 +0900

    Fixed degrees_to_radians

--- src/math_utils.c
+++ src/math_utils.c
@@ -1,3 +1,3 @@
 #include "math_utils.h"
 
-double degrees_to_radians(double degrees) { return degrees * (180.0 / M_PI); }
+double degrees_to_radians(double degrees) { return degrees * (M_PI / 180.0); }
