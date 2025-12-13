93d0cb15644214b1ec2440a9904ca26ddae6a28b
quinn-sasha <s.kuin.24.5.1435@4nonome.com>
2025-12-13 18:18:09 +0900

    Added scene generator

--- src/main.c
+++ src/main.c
@@ -5,12 +5,62 @@
 #include "object_list.h"
 #include "random_number_generator.h"
 #include "ray.h"
+#include "sphere.h"
 #include "vec3.h"
 #include <math.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+
+static t_object_list generate_random_scene(t_xorshift64_state *state) {
+  t_object_list world;
+  init_object_list(&world);
+  t_material ground_material =
+      init_lambertian_material(init_color(0.5, 0.5, 0.5));
+  object_list_insert_sphere_last(
+      &world, new_sphere(vec3_init(0, -1000, 0), 1000, ground_material));
+
+  for (int i = -11; i < 11; i++) {
+    for (int j = -11; j < 11; j++) {
+      t_vec3 center = vec3_init(i + 0.9 * random_double(state), 0.2,
+                                j + 0.9 * random_double(state));
+      if (vec3_length(vec3_sub(center, vec3_init(4, 0.2, 0))) <= 0.9)
+        continue;
+      double material_decision = random_double(state);
+      t_material sphere_material;
+      if (material_decision < 0.8) {
+        t_color albedo =
+            vec3_multiply(init_random_vec3(state), init_random_vec3(state));
+        sphere_material = init_lambertian_material(albedo);
+        object_list_insert_sphere_last(
+            &world, new_sphere(center, 0.2, sphere_material));
+        continue;
+      }
+      if (material_decision < 0.95) {
+        t_color albedo = init_random_vec3_range(state, 0.5, 1);
+        double fuzziness = random_double_range(state, 0, 0.5);
+        sphere_material = init_metal_material(albedo, fuzziness);
+        object_list_insert_sphere_last(
+            &world, new_sphere(center, 0.2, sphere_material));
+        continue;
+      }
+      sphere_material = init_dielectric_material(1.5); // glass: 1.5
+      object_list_insert_sphere_last(&world,
+                                     new_sphere(center, 0.2, sphere_material));
+    }
+  }
+  t_material material1 = init_dielectric_material(1.5);
+  object_list_insert_sphere_last(
+      &world, new_sphere(vec3_init(0, 1, 0), 1.0, material1));
+  t_material material2 = init_lambertian_material(init_color(0.4, 0.2, 0.1));
+  object_list_insert_sphere_last(
+      &world, new_sphere(vec3_init(-4, 1, 0), 1.0, material2));
+  t_material material3 = init_metal_material(init_color(0.7, 0.6, 0.5), 0.0);
+  object_list_insert_sphere_last(
+      &world, new_sphere(vec3_init(4, 1, 0), 1.0, material3));
+  return world;
+}
 
 static t_color calculate_color(t_ray ray, t_object_list *list,
                                t_xorshift64_state *state, int num_recursions) {
@@ -35,56 +85,36 @@ static t_color calculate_color(t_ray ray, t_object_list *list,
 
     if (!return_value)
       return init_color(0, 0, 0);
-    return dot_color(
+    return vec3_multiply(
         calculate_color(scattered, list, state, num_recursions - 1),
         attenuation);
   }
   t_vec3 unit_direction = vec3_normalize(ray.direction);
   double t = 0.5 * (unit_direction.y + 1.0);
   t_color white = init_color(1.0, 1.0, 1.0);
-  return add_color(scale_color(white, (1.0 - t)),
-                   scale_color(init_color(0.5, 0.7, 1.0), t));
+  return vec3_add(vec3_scale(white, (1.0 - t)),
+                  vec3_scale(init_color(0.5, 0.7, 1.0), t));
 }
 
 int main(void) {
   t_screen screen = init_screen(384, 216);
-  t_vec3 lookfrom = vec3_init(3, 3, 2);
-  t_vec3 lookat = vec3_init(0, 0, -1);
+  t_vec3 lookfrom = vec3_init(13, 2, 3);
+  t_vec3 lookat = vec3_init(0, 0, 0);
   t_vec3 view_up = vec3_init(0, 1, 0);
-  double focus_dist = vec3_length(vec3_sub(lookat, lookfrom));
+  double focus_dist = 10.0;
   t_camera camera = init_camera(lookfrom, lookat, view_up, screen.aspect_ratio,
-                                45, 1.0, focus_dist);
+                                45, 0.1, focus_dist);
+
+  t_xorshift64_state state;
+  init_xorshift64_state(&state);
+  t_object_list world = generate_random_scene(&state);
 
   int fd = STDOUT_FILENO;
   dprintf(fd, "P3\n");
   dprintf(fd, "%d %d\n255\n", screen.width, screen.height);
 
-  t_sphere *sphere1 =
-      new_sphere(vec3_init(0, 0, -1), 0.5,
-                 init_lambertian_material(init_color(0.1, 0.2, 0.5)));
-  t_sphere *sphere2 =
-      new_sphere(vec3_init(0, -100.5, -1), 100,
-                 init_lambertian_material(init_color(0.8, 0.8, 0)));
-  t_sphere *sphere3 =
-      new_sphere(vec3_init(1, 0, -1), 0.5,
-                 init_metal_material(init_color(0.8, 0.6, 0.2), 0.3));
-  t_sphere *sphere4 =
-      new_sphere(vec3_init(-1, 0, -1), 0.5, init_dielectric_material(1.5));
-  t_sphere *sphere5 =
-      new_sphere(vec3_init(-1, 0, -1), -0.45, init_dielectric_material(1.5));
-
-  t_object_list object_list;
-  init_object_list(&object_list);
-  object_list_insert_sphere_last(&object_list, sphere1);
-  object_list_insert_sphere_last(&object_list, sphere2);
-  object_list_insert_sphere_last(&object_list, sphere3);
-  object_list_insert_sphere_last(&object_list, sphere4);
-  object_list_insert_sphere_last(&object_list, sphere5);
-
   const int max_recursions = 50;
   const int num_samples_per_pixel = 100;
-  t_xorshift64_state state;
-  init_xorshift64_state(&state);
   for (int col = screen.height - 1; col >= 0; col--) {
     for (int row = 0; row < screen.width; row++) {
       dprintf(STDERR_FILENO, "Scanlines remaining: %d ", col);
@@ -94,9 +124,8 @@ int main(void) {
         double x_offset = (row + random_double(&state)) / (screen.width - 1);
         double y_offset = (col + random_double(&state)) / (screen.height - 1);
         t_ray ray = get_ray(camera, x_offset, y_offset, &state);
-        pixel_color =
-            add_color(pixel_color, calculate_color(ray, &object_list, &state,
-                                                   max_recursions));
+        pixel_color = vec3_add(
+            pixel_color, calculate_color(ray, &world, &state, max_recursions));
       }
       write_color(fd, pixel_color, num_samples_per_pixel);
     }
