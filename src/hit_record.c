a766801ed353da40782757f354619e69b300a9db
quinn-sasha <s.kuin.24.5.1435@4nonome.com>
2025-12-14 13:01:18 +0900

    Renamed a function name

--- src/hit_record.c
+++ src/hit_record.c
@@ -4,7 +4,7 @@ void set_fronts_face_and_normal_vector(t_hit_record *record, t_ray ray,
                                        t_vec3 outward_normal_vector) {
   if (dot_vec3(ray.direction, outward_normal_vector) > 0.0) {
     record->fronts_face = false;
-    record->normal_vector = vec3_scale(outward_normal_vector, -1);
+    record->normal_vector = scale_vec3(outward_normal_vector, -1);
     return;
   }
   record->fronts_face = true;
