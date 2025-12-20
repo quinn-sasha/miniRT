a5ff55f988a001772c1e9f227a66f519b3d089a4
quinn-sasha <s.kuin.24.5.1435@4nonome.com>
2025-12-20 21:58:36 +0900

    Deleted minilibx

--- include/vec3.h
+++ include/vec3.h
@@ -13,7 +13,7 @@ typedef t_vec3 t_color;
 
 t_vec3 init_vec3(double x, double y, double z);
 t_vec3 scale_vec3(t_vec3 v, double scalar);
-t_vec3 inverse_vec3(t_vec3 v);
+t_vec3 negative_vec3(t_vec3 v);
 t_vec3 divide_vec3(t_vec3 v, double scalar);
 t_vec3 add_vec3(t_vec3 a, t_vec3 b);
 t_vec3 add_triple_vec3(t_vec3 a, t_vec3 b, t_vec3 c);
@@ -23,13 +23,11 @@ double length_vec3(t_vec3 v);
 double dot_vec3(t_vec3 a, t_vec3 b);
 t_vec3 multiply_vec3(t_vec3 a, t_vec3 b);
 t_vec3 normalize_vec3(t_vec3 v);
-t_vec3 negative_vec3(t_vec3 v);
 t_vec3 cross_vec3(t_vec3 a, t_vec3 b);
 t_vec3 init_random_vec3(t_xorshift64_state *state);
 t_vec3 init_random_vec3_range(t_xorshift64_state *state, double min,
                               double max);
 t_vec3 init_random_vec3_in_unit_sphere(t_xorshift64_state *state);
-t_vec3 random_in_hemisphere(const t_vec3 normal, t_xorshift64_state *state);
 t_vec3 init_random_vec3_in_unit_circle(t_xorshift64_state *state);
 t_vec3 init_random_unit_vec3(t_xorshift64_state *state);
 t_vec3 reflect(t_vec3 incoming, t_vec3 normal);
