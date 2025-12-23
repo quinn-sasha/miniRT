6365ddf24807b6e777446c969e76fa85a9891150
quinn-sasha <s.kuin.24.5.1435@4nonome.com>
2025-12-23 15:25:48 +0900

    Added some error handling functions

--- include/error_utils.h
+++ include/error_utils.h
@@ -1,8 +1,12 @@
 #ifndef ERROR_UTILS_H
 #define ERROR_UTILS_H
 
+#include "minirt.h"
+
 #define MLX_ERROR "Minilibx library failed"
 
-void error_exit(char *message);
+void print_error(const char *message);
+void error_exit(const char *message);
+void handle_mlx_error(t_program *data);
 
 #endif // !ERROR_UTILS_H
--- src/error_utils.c
+++ src/error_utils.c
@@ -1,7 +1,16 @@
+#include "error_utils.h"
 #include "libft.h"
+#include "minilibx_utils.h"
 #include <stdlib.h>
 
-void error_exit(char *message) {
-  ft_printf("%s\n", message);
+void print_error(const char *message) { ft_printf("Error: %s\n", message); }
+
+void error_exit(const char *message) {
+  ft_printf("Error: %s\n", message);
   exit(EXIT_FAILURE);
+}
+
+void handle_mlx_error(t_program *data) {
+  destroy_mlx_resources_if_allocated(data);
+  error_exit(MLX_ERROR);
 }
