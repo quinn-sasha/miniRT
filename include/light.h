ba101bd8465c431a6e57b65f25067e944c1987a1
quinn-sasha <s.kuin.24.5.1435@4nonome.com>
2025-12-23 15:24:19 +0900

    Added light.h

---
+++ include/light.h
@@ -0,0 +1,20 @@
+#ifndef LIGHT_H
+#define LIGHT_H
+
+#include "vec3.h"
+
+typedef struct s_light {
+  t_vec3 pos;
+  double brightness_ratio;
+  t_color color;
+} t_light;
+
+typedef struct s_ambient {
+  double ratio;
+  t_color color;
+} t_ambient;
+
+void init_light(t_light light);
+void init_ambient(t_ambient);
+
+#endif
