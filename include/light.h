907d1fba88865dadbcd922f9acc135f7df512575
quinn-sasha <s.kuin.24.5.1435@4nonome.com>
2025-12-24 17:27:25 +0900

    Added light.h

--- include/light.h
+++ include/light.h
@@ -14,7 +14,8 @@ typedef struct s_ambient {
   t_color color;
 } t_ambient;
 
-void init_light(t_light light);
-void init_ambient(t_ambient);
+// TODO: 仮に宣言だけしたので、実装はしてない
+t_light init_light(t_vec3 pos, double brightness_ratio, t_color color);
+t_ambient init_ambient(double brightness_ratio, t_color color);
 
 #endif
