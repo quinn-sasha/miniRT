cf032556f71daf213674fccd57fe1e953fe65dbe
quinn-sasha <s.kuin.24.5.1435@4nonome.com>
2025-12-25 15:34:31 +0900

    Make ft_isspace() in ft_atoi.c static

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -12,48 +12,44 @@
 
 #include "libft.h"
 
-int	ft_isspace(int c)
-{
-	if (c == ' ')
-		return (1);
-	if (c == '\f')
-		return (1);
-	if (c == '\n')
-		return (1);
-	if (c == '\r')
-		return (1);
-	if (c == '\t')
-		return (1);
-	if (c == '\v')
-		return (1);
-	return (0);
+static int ft_isspace(int c) {
+  if (c == ' ')
+    return (1);
+  if (c == '\f')
+    return (1);
+  if (c == '\n')
+    return (1);
+  if (c == '\r')
+    return (1);
+  if (c == '\t')
+    return (1);
+  if (c == '\v')
+    return (1);
+  return (0);
 }
 
-int	ft_atoi(const char *str)
-{
-	long long int	result;
-	int				sign;
-	int				i;
+int ft_atoi(const char *str) {
+  long long int result;
+  int sign;
+  int i;
 
-	i = 0;
-	while (ft_isspace(str[i]))
-		i++;
-	sign = 1;
-	if (str[i] == '+' || str[i] == '-')
-	{
-		if (str[i] == '-')
-			sign = -1;
-		i++;
-	}
-	result = 0;
-	while (str[i])
-	{
-		if (!ft_isdigit(str[i]))
-			return ((int)(sign * result));
-		result = (str[i] - '0') + result * 10;
-		i++;
-	}
-	return ((int)(sign * result));
+  i = 0;
+  while (ft_isspace(str[i]))
+    i++;
+  sign = 1;
+  if (str[i] == '+' || str[i] == '-') {
+    if (str[i] == '-')
+      sign = -1;
+    i++;
+  }
+  result = 0;
+  while (str[i]) {
+    if (!ft_isdigit(str[i]))
+      return ((int)(sign * result));
+    result = (str[i] - '0') + result * 10;
+    i++;
+  }
+  return ((int)(sign * result));
 }
 
 /*
@@ -62,10 +58,10 @@ int	ft_atoi(const char *str)
 #include <stdlib.h>
 
 int	main(int argc, char *argv[]) {
-	if (argc != 2)
-		return (1);
-	printf("ft_atoi: %d\n", ft_atoi(argv[1]));
-	printf("atoi: %d\n", atoi(argv[1]));
-	assert(ft_atoi(argv[1]) == atoi(argv[1]));
+        if (argc != 2)
+                return (1);
+        printf("ft_atoi: %d\n", ft_atoi(argv[1]));
+        printf("atoi: %d\n", atoi(argv[1]));
+        assert(ft_atoi(argv[1]) == atoi(argv[1]));
 }
 */
