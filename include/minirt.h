b14c3b3493d20f7ad64ed180fa623a8298c6aab8
Inahara Kota <ikota@c4r3s15.42tokyo.jp>
2025-12-26 19:09:48 +0900

    calculate_colorのファイル分割とt_programにnum_recursiosとnum_samples_per_pixelを追加

--- include/minirt.h
+++ include/minirt.h
@@ -1,23 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minirt.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ikota <ikota@student.42tokyo.jp>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/26 19:06:45 by ikota             #+#    #+#             */
+/*   Updated: 2025/12/26 19:06:46 by ikota            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINIRT_H
 # define MINIRT_H
 
-#include "camera.h"
-#include "img.h"
-#include "light.h"
-#include "object_list.h"
-#include "light.h"
+# include "camera.h"
+# include "img.h"
+# include "light.h"
+# include "object_list.h"
 
-#define WIDTH 384
-#define HEIGHT 216
+# define WIDTH 384
+# define HEIGHT 216
 
-typedef struct s_program {
-  void *mlx;
-  void *window;
-  t_img img;
-  t_scene_object head;
-  t_camera camera;
-  t_ambient ambient;
-  t_light light;
-} t_program;
+typedef struct s_program
+{
+	void			*mlx;
+	void			*window;
+	t_img			img;
+	t_scene_object	head;
+	t_camera		camera;
+	t_ambient		ambient;
+	t_light			light;
+	int				max_recursions;
+	int				num_samples_per_pixel;
+}					t_program;
 
 #endif
