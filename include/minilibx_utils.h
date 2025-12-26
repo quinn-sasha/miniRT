bce48dbb9cd204f0abea97f746ae0da5bedbe5fd
quinn-sasha <s.kuin.24.5.1435@4nonome.com>
2025-12-26 18:28:38 +0900

    Fixed norm in minilibx_utils

--- include/minilibx_utils.h
+++ include/minilibx_utils.h
@@ -1,22 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minilibx_utils.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: squinn <squinn@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/26 18:27:38 by squinn            #+#    #+#             */
+/*   Updated: 2025/12/26 18:27:39 by squinn           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINILIBX_UTILS_H
-#define MINILIBX_UTILS_H
+# define MINILIBX_UTILS_H
 
-#define KEYPRESSMASK 1
-#define KEYPRESS 2
-#define DESTROYNOTIFY 17
-#ifdef __linux__
-#define XK_ESCAPE 0xff1b
-#else
-#define XK_ESCAPE 53
-#endif
+# define KEYPRESSMASK 1
+# define KEYPRESS 2
+# define DESTROYNOTIFY 17
+# ifdef __linux__
+#  define XK_ESCAPE 0xff1b
+# else
+#  define XK_ESCAPE 53
+# endif
 
-#include "img.h"
-#include "minirt.h"
-#include <stdint.h>
+# include "img.h"
+# include "minirt.h"
+# include <stdint.h>
 
-void set_pixel_color(int x, int y, t_img *img, uint32_t color);
-void init_mlx_resources(t_program *data);
-void destroy_mlx_resources_if_allocated(t_program *data);
-void set_mlx_hooks(t_program *data);
+void	set_pixel_color(int x, int y, t_img *img, uint32_t color);
+void	init_mlx_resources(t_program *data);
+void	destroy_mlx_resources_if_allocated(t_program *data);
+void	set_mlx_hooks(t_program *data);
 
 #endif // !MINILIBX_UTILS_H
--- src/minilibx_utils.c
+++ src/minilibx_utils.c
@@ -1,59 +1,76 @@
-#include "minilibx_utils.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minilibx_utils.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: squinn <squinn@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/26 18:25:50 by squinn            #+#    #+#             */
+/*   Updated: 2025/12/26 18:27:20 by squinn           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "error_utils.h"
+#include "minilibx_utils.h"
 #include "mlx.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 // parse で動的に確保したものはfree()を呼び出さずに終了する
-void init_mlx_resources(t_program *data) {
-  t_img *image;
+void	init_mlx_resources(t_program *data)
+{
+	t_img	*image;
 
-  data->mlx = NULL;
-  data->window = NULL;
-  data->img.mlx_img = NULL;
-  data->mlx = mlx_init();
-  if (!data->mlx)
-    error_exit(MLX_ERROR);
-  data->window = mlx_new_window(data->mlx, WIDTH, HEIGHT, "miniRT");
-  if (!data->window)
-    handle_mlx_error(data);
-  data->img.mlx_img = mlx_new_image(data->mlx, WIDTH, HEIGHT);
-  if (!data->img.mlx_img)
-    handle_mlx_error(data);
-  image = &data->img;
-  image->address = mlx_get_data_addr(image->mlx_img, &image->bits_per_pixel,
-                                     &image->row_size, &image->endian);
-  (void)image->endian;
+	data->mlx = NULL;
+	data->window = NULL;
+	data->img.mlx_img = NULL;
+	data->mlx = mlx_init();
+	if (!data->mlx)
+		error_exit(MLX_ERROR);
+	data->window = mlx_new_window(data->mlx, WIDTH, HEIGHT, "miniRT");
+	if (!data->window)
+		handle_mlx_error(data);
+	data->img.mlx_img = mlx_new_image(data->mlx, WIDTH, HEIGHT);
+	if (!data->img.mlx_img)
+		handle_mlx_error(data);
+	image = &data->img;
+	image->address = mlx_get_data_addr(image->mlx_img, &image->bits_per_pixel,
+			&image->row_size, &image->endian);
+	(void)image->endian;
 }
 
-void destroy_mlx_resources_if_allocated(t_program *data) {
-  if (data->img.mlx_img) {
-    mlx_destroy_image(data->mlx, data->img.mlx_img);
-  }
-  if (data->window) {
-    mlx_destroy_window(data->mlx, data->window);
-  }
-#ifdef __linux__
-  mlx_destroy_display(data->mlx);
-#endif
-  free(data->mlx);
+void	destroy_mlx_resources_if_allocated(t_program *data)
+{
+	if (data->img.mlx_img)
+	{
+		mlx_destroy_image(data->mlx, data->img.mlx_img);
+	}
+	if (data->window)
+	{
+		mlx_destroy_window(data->mlx, data->window);
+	}
+	mlx_destroy_display(data->mlx);
+	free(data->mlx);
 }
 
-int destroy_window(t_program *data) {
-  mlx_destroy_window(data->mlx, data->window);
-  data->window = NULL;
-  destroy_mlx_resources_if_allocated(data);
-  destroy_object_list(&data->head);
-  exit(EXIT_SUCCESS);
+int	destroy_window(t_program *data)
+{
+	mlx_destroy_window(data->mlx, data->window);
+	data->window = NULL;
+	destroy_mlx_resources_if_allocated(data);
+	destroy_object_list(&data->head);
+	exit(EXIT_SUCCESS);
 }
 
-static int handle_keypress(int keysymbol, t_program *data) {
-  if (keysymbol == XK_ESCAPE)
-    return (destroy_window(data));
-  return (0);
+static int	handle_keypress(int keysymbol, t_program *data)
+{
+	if (keysymbol == XK_ESCAPE)
+		return (destroy_window(data));
+	return (0);
 }
 
-void set_mlx_hooks(t_program *data) {
-  mlx_hook(data->window, KEYPRESS, KEYPRESSMASK, handle_keypress, data);
-  mlx_hook(data->window, DESTROYNOTIFY, 0, destroy_window, data);
+void	set_mlx_hooks(t_program *data)
+{
+	mlx_hook(data->window, KEYPRESS, KEYPRESSMASK, handle_keypress, data);
+	mlx_hook(data->window, DESTROYNOTIFY, 0, destroy_window, data);
 }
