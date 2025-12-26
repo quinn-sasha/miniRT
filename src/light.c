82526957c87863b8329a34a3463a5b67768f199b
quinn-sasha <s.kuin.24.5.1435@4nonome.com>
2025-12-26 15:17:10 +0900

    Fixed norm in light

--- src/light.c
+++ src/light.c
@@ -1,16 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   light.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: squinn <squinn@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/26 15:16:24 by squinn            #+#    #+#             */
+/*   Updated: 2025/12/26 15:16:25 by squinn           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "light.h"
 
-t_light init_light(t_vec3 pos, double brightness_ratio, t_color color) {
-  t_light light;
-  light.pos = pos;
-  light.brightness_ratio = brightness_ratio;
-  light.color = color;
-  return light;
+t_light	init_light(t_vec3 pos, double brightness_ratio, t_color color)
+{
+	t_light	light;
+
+	light.pos = pos;
+	light.brightness_ratio = brightness_ratio;
+	light.color = color;
+	return (light);
 }
 
-t_ambient init_ambient(double brightness_ratio, t_color color) {
-  t_ambient ambient;
-  ambient.ratio = brightness_ratio;
-  ambient.color = color;
-  return ambient;
+t_ambient	init_ambient(double brightness_ratio, t_color color)
+{
+	t_ambient	ambient;
+
+	ambient.ratio = brightness_ratio;
+	ambient.color = color;
+	return (ambient);
 }
