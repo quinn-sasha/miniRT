9eb408f9c0a898861950ca4206fe4de9ff26395c
quinn-sasha <s.kuin.24.5.1435@4nonome.com>
2025-12-26 19:00:10 +0900

    Fixed norm in parse

--- src/parse/parse_cylinder.c
+++ src/parse/parse_cylinder.c
@@ -18,44 +18,47 @@
 #include "utilities.h"
 #include <math.h>
 
-static int parse_attrs(char **params, t_vec3 *vectors, double *dimensions) {
-  if (parse_vector(params[1], &vectors[0]) == PARSE_FAILED)
-    return (PARSE_FAILED);
-  if (parse_unit_vector(params[2], &vectors[1]) == PARSE_FAILED)
-    return (PARSE_FAILED);
-  if (parse_double_range(params[3], &dimensions[0], 0, INFINITY) ==
-      PARSE_FAILED)
-    return (PARSE_FAILED);
-  if (parse_double_range(params[4], &dimensions[1], 0, INFINITY) ==
-      PARSE_FAILED)
-    return (PARSE_FAILED);
-  return (PARSE_SUCCESS);
+static int	parse_attrs(char **params, t_vec3 *vectors, double *dimensions)
+{
+	if (parse_vector(params[1], &vectors[0]) == PARSE_FAILED)
+		return (PARSE_FAILED);
+	if (parse_unit_vector(params[2], &vectors[1]) == PARSE_FAILED)
+		return (PARSE_FAILED);
+	if (parse_double_range(params[3], &dimensions[0], 0,
+			INFINITY) == PARSE_FAILED)
+		return (PARSE_FAILED);
+	if (parse_double_range(params[4], &dimensions[1], 0,
+			INFINITY) == PARSE_FAILED)
+		return (PARSE_FAILED);
+	return (PARSE_SUCCESS);
 }
 
 // Return PARSE_SUCCESS if succeed, otherwise PARSE_FAILED
-static int parse_cylinder_helper(const char *line, t_program *data) {
-  char **params;
-  t_vec3 vectors[2];
-  double dimensions[2];
-  t_color albedo;
-  t_cylinder *cylinder;
+static int	parse_cylinder_helper(const char *line, t_program *data)
+{
+	char		**params;
+	t_vec3		vectors[2];
+	double		dimensions[2];
+	t_color		albedo;
+	t_cylinder	*cylinder;
 
-  params = ft_split(line, ' ');
-  if (count_strings(params) != 7)
-    return (handle_parse_error(params));
-  if (parse_attrs(params, vectors, dimensions) == PARSE_FAILED)
-    return (handle_parse_error(params));
-  if (parse_color(params[5], &albedo) == PARSE_FAILED)
-    return (handle_parse_error(params));
-  cylinder =
-      new_cylinder(vectors, dimensions, parse_material(params[6], albedo));
-  add_cylinder(&data->head, cylinder);
-  free_strings(params);
-  return (PARSE_SUCCESS);
+	params = ft_split(line, ' ');
+	if (count_strings(params) != 7)
+		return (handle_parse_error(params));
+	if (parse_attrs(params, vectors, dimensions) == PARSE_FAILED)
+		return (handle_parse_error(params));
+	if (parse_color(params[5], &albedo) == PARSE_FAILED)
+		return (handle_parse_error(params));
+	cylinder = new_cylinder(vectors, dimensions, parse_material(params[6],
+				albedo));
+	add_cylinder(&data->head, cylinder);
+	free_strings(params);
+	return (PARSE_SUCCESS);
 }
 
-int parse_cylinder(const char *line, t_program *data) {
-  if (parse_cylinder_helper(line, data) == PARSE_FAILED)
-    return (error_return("Invalid cylinder setting", PARSE_FAILED));
-  return (PARSE_SUCCESS);
+int	parse_cylinder(const char *line, t_program *data)
+{
+	if (parse_cylinder_helper(line, data) == PARSE_FAILED)
+		return (error_return("Invalid cylinder setting", PARSE_FAILED));
+	return (PARSE_SUCCESS);
 }
--- src/parse/parse_utils.c
+++ src/parse/parse_utils.c
@@ -10,80 +10,88 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "parse_utils.h"
 #include "color.h"
 #include "error_utils.h"
 #include "ft_atof.h"
 #include "libft.h"
 #include "material.h"
 #include "parse.h"
+#include "parse_utils.h"
 #include "utilities.h"
 #include "vec3.h"
 
 #define EPSILON 1e-3
 
 // Return PARSE_SUCCESS if succeed, otherwise PARSE_FAILED
-int parse_color(const char *str, t_color *color) {
-  char **colors;
-  int i;
-  double r;
-  double g;
-  double b;
+int	parse_color(const char *str, t_color *color)
+{
+	char	**colors;
+	int		i;
+	double	r;
+	double	g;
+	double	b;
 
-  colors = ft_split(str, ',');
-  if (count_strings(colors) != 3)
-    return (handle_parse_error(colors));
-  i = 0;
-  while (colors[i]) {
-    if (!is_unsigned_int(colors[i]))
-      return (handle_parse_error(colors));
-    i++;
-  }
-  r = (double)ft_atoi(colors[0]) / 255;
-  g = (double)ft_atoi(colors[1]) / 255;
-  b = (double)ft_atoi(colors[2]) / 255;
-  *color = init_color(r, g, b);
-  free_strings(colors);
-  return (PARSE_SUCCESS);
+	colors = ft_split(str, ',');
+	if (count_strings(colors) != 3)
+		return (handle_parse_error(colors));
+	i = 0;
+	while (colors[i])
+	{
+		if (!is_unsigned_int(colors[i]))
+			return (handle_parse_error(colors));
+		i++;
+	}
+	r = (double)ft_atoi(colors[0]) / 255;
+	g = (double)ft_atoi(colors[1]) / 255;
+	b = (double)ft_atoi(colors[2]) / 255;
+	*color = init_color(r, g, b);
+	free_strings(colors);
+	return (PARSE_SUCCESS);
 }
 
 // Return PARSE_SUCCESS if succeed, otherwise PARSE_FAILED
-int parse_vector(const char *str, t_vec3 *output) {
-  char **coordinates;
-  int i;
+int	parse_vector(const char *str, t_vec3 *output)
+{
+	char	**coordinates;
+	int		i;
 
-  coordinates = ft_split(str, ',');
-  if (count_strings(coordinates) != 3)
-    return (handle_parse_error(coordinates));
-  i = 0;
-  while (coordinates[i]) {
-    if (!is_floating_point(coordinates[i]))
-      return (handle_parse_error(coordinates));
-    i++;
-  }
-  output->x = ft_atof(coordinates[0]);
-  output->y = ft_atof(coordinates[1]);
-  output->z = ft_atof(coordinates[2]);
-  free_strings(coordinates);
-  return (PARSE_SUCCESS);
+	coordinates = ft_split(str, ',');
+	if (count_strings(coordinates) != 3)
+		return (handle_parse_error(coordinates));
+	i = 0;
+	while (coordinates[i])
+	{
+		if (!is_floating_point(coordinates[i]))
+			return (handle_parse_error(coordinates));
+		i++;
+	}
+	output->x = ft_atof(coordinates[0]);
+	output->y = ft_atof(coordinates[1]);
+	output->z = ft_atof(coordinates[2]);
+	free_strings(coordinates);
+	return (PARSE_SUCCESS);
 }
 
 // Return PARSE_SUCCESS if succeed, otherwise PARSE_FAILED
-int parse_unit_vector(const char *str, t_vec3 *output) {
-  if (parse_vector(str, output) == PARSE_FAILED)
-    return (PARSE_FAILED);
-  if (ft_fabs(length_squared_vec3(*output) - 1.0) > EPSILON)
-    return (PARSE_FAILED);
-  return (PARSE_SUCCESS);
+int	parse_unit_vector(const char *str, t_vec3 *output)
+{
+	if (parse_vector(str, output) == PARSE_FAILED)
+		return (PARSE_FAILED);
+	if (ft_fabs(length_squared_vec3(*output) - 1.0) > EPSILON)
+		return (PARSE_FAILED);
+	return (PARSE_SUCCESS);
 }
 
 // typeが不明な場合もlambertianを返す
-t_material parse_material(const char *type, t_color albedo) {
-  if (ft_strcmp(type, "metal") == 0) {
-    return (init_metal_material(albedo, 0));
-  }
-  if (ft_strcmp(type, "dielectric") == 0) {
-    return (init_dielectric_material(1.5));
-  }
-  return (init_lambertian_material(albedo));
+t_material	parse_material(const char *type, t_color albedo)
+{
+	if (ft_strcmp(type, "metal") == 0)
+	{
+		return (init_metal_material(albedo, 0));
+	}
+	if (ft_strcmp(type, "dielectric") == 0)
+	{
+		return (init_dielectric_material(1.5));
+	}
+	return (init_lambertian_material(albedo));
 }
