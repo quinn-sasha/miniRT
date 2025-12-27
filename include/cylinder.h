a70f4a660bbbd53b9ed0b2407758cf1459beafaa
Inahara Kota <ikota@c6r3s4.42tokyo.jp>
2025-12-27 14:15:54 +0900

    hit_cylinder.cの追加

--- include/cylinder.h
+++ include/cylinder.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cylinder.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: ikota <ikota@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/25 20:26:19 by ikota             #+#    #+#             */
-/*   Updated: 2025/12/27 01:10:01 by root             ###   ########.fr       */
+/*   Updated: 2025/12/27 14:02:56 by ikota            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,9 @@ typedef struct s_basis {
 
 t_cylinder *new_cylinder(t_vec3 *vectors, double *dimensions,
                          t_material material);
+
+bool hit_cylinder_side_cap(t_ray local_ray, t_range range,
+                                  t_hit_record *record, const t_cylinder *cyl);
 
 bool hit_cylinder(const t_ray ray, t_range range, t_hit_record *hit_rec,
                   const t_cylinder *cyl);
